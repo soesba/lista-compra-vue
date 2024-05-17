a01146578f70c54255e3043174f12f29b0053adc
Sonia Esquer <sonia.esquer-banos@capgemini.com>
2024-05-17 09:19:48 +0200

    master Bases proyecto. Vuetify

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <app-bar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { AppBar } from '@/components/index'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    AppBar
+  },
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+createApp(App).
+  use(store).
+  use(router).
+  use(vuetify).
+  mount("#app")
