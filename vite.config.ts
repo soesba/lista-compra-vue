b189891e8cf8d8dd902199e3057343f2e1413433
Sonia Esquer <sonia.esquer@gmail.com>
2025-10-01 14:15:58 +0200

    master Restauramos

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
   plugins: [
     vue(),
     vueDevTools()
   ],
   resolve: {
     alias: {
-      '@': path.resolve('.', 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   // define: {
