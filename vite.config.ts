c6369fd4af56fd5f3fac78f65b59f8a785468e41
Sonia Esquer <sonia.esquer@gmail.com>
2025-10-01 10:19:36 +0200

    master Alias @

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +16,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve(__dirname, 'src')
     }
   },
   // define: {
