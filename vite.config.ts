d0d4ce004e709ef3d78f1601dedada5f931519b4
Sonia Esquer <sonia.esquer@gmail.com>
2025-10-02 06:07:32 +0200

    deployment Pruebas correcciones rutas

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+console.log('Ruta raiz es', import.meta.url)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('src', import.meta.url))
     }
   },
   // define: {
